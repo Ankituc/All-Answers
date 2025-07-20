cad7dc493d054f44fcfa9d01be70eea3d6ddfeae
Ankituc <150525427+Ankituc@users.noreply.github.com>
2025-07-20 10:42:06 +0530

    inkedlist_rev_middle.java

---
+++ 28/linkedlist_rev_middle.java
@@ -0,0 +1,50 @@
+//Linked list
+class Solution {
+    static boolean searchKey(int n, Node head, int key) {
+        Node current = head;
+        while (current != null) {
+            if (current.data == key) {
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+}
+
+
+//reverse
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+}
+
+
+//middle linked list
+
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+
